871f4c301efba795fa02c5968489397765618347
Igor Krivoy <javadevik@gmail.com>
2022-05-02 12:05:11 +0300

    add repositoty User

---
+++ src/main/java/com/ua/ToDo/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.ua.ToDo.repository;
+
+import com.ua.ToDo.entity.UserEntity;
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserRepository extends CrudRepository<UserEntity, Long> {
+    UserEntity findByUsername(String username);
+}
